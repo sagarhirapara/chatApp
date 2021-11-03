6b0bd6f55dc36d1a8cc9330cd2ca1d03c30833bc
sagarhirapara <86096538+sagarhirapara@users.noreply.github.com>
2021-11-03 20:50:22 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect("localhost","root","","chat") or die("connection failed");
+$conn = mysqli_connect("remotemysql.com","XUKFpDEXRl"," Zrz35N3IFG","XUKFpDEXRl") or die("connection failed");
 ?>
